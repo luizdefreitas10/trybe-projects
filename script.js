dff1a0f8621e634e1b8a7c7992b3bd8e55341caa
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-04-20 20:30:52 -0300

    Concluindo requisitos 4 e 5.

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ corAmarela.style.backgroundColor = "yellow";
 let corPreta = document.getElementsByClassName("color")[0];
 corPreta.style.backgroundColor = "black";
 
+const pixelBoard = document.querySelector("#pixel-board");
+
+// function gerarQuadro(tamanho) {
+//     for (let linha = 0; linha < tamanho; linha += 1) {
+//         const quadroPixel = document.createElement("div");
+//         quadroPixel.className = "pixel";
+//         pixelBoard.appendChild(quadroPixel);
+//         for (let coluna = 0; coluna < tamanho; coluna += 1) {
+//         const pixel = document.createElement("div");
+//         pixel.className = "pixel";
+//         quadroPixel.appendChild(pixel);
+//         }
+//     }
+// }
+
+// gerarQuadro(5); nao consegui terminar de implementar o correto funcionamento da função, por isso resolvi deixar o codigo comentado e resolvi estruturar os pixels no proprio HTML.
+
