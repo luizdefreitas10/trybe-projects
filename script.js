4503cc8379ad507d4f0a72869f8745eb2ef0cdf5
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-04-20 15:35:46 -0300

    Concluindo requisitos 1, 2 e 3.

---
+++ script.js
@@ -0,0 +1,12 @@
+let corAzul = document.getElementsByClassName("color")[3];
+corAzul.style.backgroundColor = "blue";
+
+let corVermelha = document.getElementsByClassName("color")[1];
+corVermelha.style.backgroundColor = "red";
+
+let corAmarela = document.getElementsByClassName("color")[2];
+corAmarela.style.backgroundColor = "yellow";
+
+let corPreta = document.getElementsByClassName("color")[0];
+corPreta.style.backgroundColor = "black";
+
