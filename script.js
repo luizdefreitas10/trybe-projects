5425eb62c17e8e6e14588c02b00165e9079dd23a
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-04-24 20:19:16 -0300

    Concluindo requisito 8

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const createButton = document.getElementById("criar-tarefa");
 const listItens = document.getElementById("lista-tarefas");
 const inputItem = document.getElementById("texto-tarefa");
 
+
 function createLi() {
     createButton.addEventListener('click', function () {
         const newLi = document.createElement("li");
-        newLi.className = "liClass"
+        //newLi.className = "liClass"
         listItens.appendChild(newLi);
         newLi.innerHTML = inputItem.value;
         inputItem.value = "";
@@ -13,19 +14,31 @@ function createLi() {
 
 }
 
-function changeTextColor () {
-    listItens.addEventListener ('click', function (event) {
-        event.target.style.backgroundColor = "grey";
+function changeTextColor() {
+ const selectedLi = document.querySelectorAll(".selected");   listItens.addEventListener('click', function (event) {
+        event.target.className = "selected"
+        //event.target.style.backgroundColor = "grey";
     })
 }
 
-function displayAlert () {
+function selectClass() {
+    listItens.addEventListener('click', function (event) {
+        const selectedLi = document.querySelectorAll(".selected");
+        for (let index = 0; index < selectedLi.length; index += 1) {
+            selectedLi[index].classList.remove("selected");
+        }
+        event.target.classList.add("selected");
+    });
+}
+
+function displayAlert() {
     alert("Page loaded.")
 }
 
 window.onload = function () {
-    createLi ();
-    displayAlert ();
-    changeTextColor ();
+    createLi();
+    displayAlert();
+    changeTextColor();
+    selectClass();
 }
 
