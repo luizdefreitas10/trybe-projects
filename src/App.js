2185930eaf5dee99e3907d1d58ec058ce9440ed6
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-07-01 15:33:44 -0300

    Concluindo requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
+import Album from './pages/Album';
 
 class App extends React.Component {
   render() {
-    return (<p>Trybe Tunes</p>);
+    return (
+      <BrowserRouter>
+        <div>
+          <Login />
+          <Search />
+          <Profile />
+          <ProfileEdit />
+          <NotFound />
+          <Favorites />
+          <Album />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
