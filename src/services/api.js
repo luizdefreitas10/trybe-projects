5aa8b461a63153417b97835773fa04cbb0968e8c
VictorSilva27 <victoradaosilva2@gmail.com>
2022-07-07 16:39:44 -0300

    Requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -8,22 +8,22 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-  if (categoryId && !query) {
-    const URL_CATEGORIES = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
-    const response = await fetch(URL_CATEGORIES);
-    const categories = await response.json();
-    return categories;
-  }
-  if (!categoryId && query) {
-    const URL_QUERY = `https://api.mercadolibre.com/sites/MLB/search?q=$${query}`;
-    const response = await fetch(URL_QUERY);
-    const querySearch = await response.json();
-    return querySearch;
-  }
-  if (categoryId && query) {
-    const URL = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
-    const response = await fetch(URL);
-    const catQuerySearch = await response.json();
-    return catQuerySearch;
-  }
+  // if (categoryId && !query) {
+  //   const URL_CATEGORIES = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
+  //   const response = await fetch(URL_CATEGORIES);
+  //   const categories = await response.json();
+  //   return categories;
+  // }
+  // if (!categoryId && query) {
+  //   const URL_QUERY = `https://api.mercadolibre.com/sites/MLB/search?q=$${query}`;
+  //   const response = await fetch(URL_QUERY);
+  //   const querySearch = await response.json();
+  //   return querySearch;
+  // }
+  // if (categoryId && query) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
+  const response = await fetch(URL);
+  const catQuerySearch = await response.json();
+  return catQuerySearch;
+  // }
 }
