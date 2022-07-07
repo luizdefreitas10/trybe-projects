9fd490ed2dd7a3124c90348a977d68a93de0dfd0
Daniel Campelo <danielcampelo159@gmail.com>
2022-07-07 18:35:55 -0300

    Realiza requisito 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './components/Home';
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <BrowserRouter>
+        <Route path="/" exact component={ Home } />
+      </BrowserRouter>
     );
   }
 }
