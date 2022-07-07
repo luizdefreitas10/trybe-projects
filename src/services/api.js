3e5386cc7924c3837426270159ee53fce082b545
Daniel Campelo <danielcampelo159@gmail.com>
2022-07-07 16:55:19 -0300

    Corrigindo lint req1

--- src/services/api.js
+++ src/services/api.js
@@ -1,54 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
-  return fetch('https://api.mercadolibre.com/sites/MLB/categories').then((search) => search.json()).then(((data) => data));
-  // const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
-  // const response = await fetch(URL);
-  // const categories = response.json();
-  // return categories;
+  return fetch("https://api.mercadolibre.com/sites/MLB/categories")
+    .then((search) => search.json())
+    .then((data) => data);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`).then((search) => search.json()).then((data) => data);
-  }
-
-  // if (categoryId && !query) {
-  //   const URL_CATEGORIES = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
-  //   const response = await fetch(URL_CATEGORIES);
-  //   const categories = response.json();
-
-  // if (categoryId && !query) {
-  //   const URL_CATEGORIES = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
-  //   const response = await fetch(URL_CATEGORIES);
-  //   const categories = await response.json();
-
-  //   return categories;
-  // }
-  // if (!categoryId && query) {
-  //   const URL_QUERY = `https://api.mercadolibre.com/sites/MLB/search?q=$${query}`;
-  //   const response = await fetch(URL_QUERY);
-  //   const querySearch = response.json();
-  //   return querySearch;
-  // }
-  // if (categoryId && query) {
-  //   const URL = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
-  //   const response = await fetch(URL);
-  //   const catQuerySearch = response.json();
-  //   return catQuerySearch;
-  // }
-
-  // return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`)
-  //   .then((data) => data.json());
-//}
-
-  //   const querySearch = await response.json();
-  //   return querySearch;
-  // }
-  // if (categoryId && query) {
-  // const URL = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
-  // const response = await fetch(URL);
-  // const catQuerySearch = await response.json();
-  // return catQuerySearch;
-  // }
-// }
-
+  return fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+  )
+    .then((search) => search.json())
+    .then((data) => data);
+}
