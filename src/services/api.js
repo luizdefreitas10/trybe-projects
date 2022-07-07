d6db62e53a1dcda11674112d352e3e9260edba64
Daniel Campelo <danielcampelo159@gmail.com>
2022-07-07 16:58:47 -0300

    Corrigindo lint req1 denovo

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 export async function getCategories() {
-  return fetch("https://api.mercadolibre.com/sites/MLB/categories")
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
     .then((search) => search.json())
     .then((data) => data);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
   )
     .then((search) => search.json())
     .then((data) => data);
