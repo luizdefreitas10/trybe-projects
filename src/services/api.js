65c849f882c28544fed57a07abfc9f21b96d076b
VictorSilva27 <victoradaosilva2@gmail.com>
2022-07-07 16:29:51 -0300

    Requisito 01 Corrigido

--- src/services/api.js
+++ src/services/api.js
@@ -11,22 +11,19 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   if (categoryId && !query) {
     const URL_CATEGORIES = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
     const response = await fetch(URL_CATEGORIES);
-    const categories = response.json();
+    const categories = await response.json();
     return categories;
   }
   if (!categoryId && query) {
     const URL_QUERY = `https://api.mercadolibre.com/sites/MLB/search?q=$${query}`;
     const response = await fetch(URL_QUERY);
-    const querySearch = response.json();
+    const querySearch = await response.json();
     return querySearch;
   }
   if (categoryId && query) {
     const URL = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
     const response = await fetch(URL);
-    const catQuerySearch = response.json();
+    const catQuerySearch = await response.json();
     return catQuerySearch;
   }
-
-  // return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`)
-  //   .then((data) => data.json());
 }
