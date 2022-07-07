8207ef9083f059af3a652ebcf97755a547fcf0cb
Daniel Campelo <danielcampelo159@gmail.com>
2022-07-07 16:45:17 -0300

    Corrigindo req1

--- src/services/api.js
+++ src/services/api.js
@@ -6,27 +6,30 @@ export async function getCategories() {
   return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
+export async function getProductsFromCategoryAndQuery(id, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-  if (categoryId && !query) {
-    const URL_CATEGORIES = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
-    const response = await fetch(URL_CATEGORIES);
-    const categories = response.json();
-    return categories;
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${id}&q=${query}`).then((search) => search.json()).then((data) => data);
   }
-  if (!categoryId && query) {
-    const URL_QUERY = `https://api.mercadolibre.com/sites/MLB/search?q=$${query}`;
-    const response = await fetch(URL_QUERY);
-    const querySearch = response.json();
-    return querySearch;
-  }
-  if (categoryId && query) {
-    const URL = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
-    const response = await fetch(URL);
-    const catQuerySearch = response.json();
-    return catQuerySearch;
-  }
+
+  // if (categoryId && !query) {
+  //   const URL_CATEGORIES = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
+  //   const response = await fetch(URL_CATEGORIES);
+  //   const categories = response.json();
+  //   return categories;
+  // }
+  // if (!categoryId && query) {
+  //   const URL_QUERY = `https://api.mercadolibre.com/sites/MLB/search?q=$${query}`;
+  //   const response = await fetch(URL_QUERY);
+  //   const querySearch = response.json();
+  //   return querySearch;
+  // }
+  // if (categoryId && query) {
+  //   const URL = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
+  //   const response = await fetch(URL);
+  //   const catQuerySearch = response.json();
+  //   return catQuerySearch;
+  // }
 
   // return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`)
   //   .then((data) => data.json());
-}
+//}
