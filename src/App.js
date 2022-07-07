a53fbd5990eb08ebbdf113bb711aa73d993e688b
Daniel Campelo <danielcampelo159@gmail.com>
2022-07-07 19:03:46 -0300

    Realiza requisito 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import Cart from './components/Cart';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route path="/" exact component={ Home } />
+        <Route path="/cart" component={ Cart } />
       </BrowserRouter>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from './Button';
+import './Home.css';
 
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="home-container">
         <input type="text" />
+        <Link to="/cart" data-testid="shopping-cart-button"><Button /></Link>
         <p
           data-testid="home-initial-message"
         >
