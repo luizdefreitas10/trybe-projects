1b90be41f735f9308bc2743fb43c1017af169828
Daniel Campelo <danielcampelo159@gmail.com>
2022-07-07 21:07:32 -0300

    commit

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class ProductList extends Component {
+  render() {
+    const { propObjResults } = this.props;
+    return (
+      <div>aaa</div>
+    );
+  }
+}
