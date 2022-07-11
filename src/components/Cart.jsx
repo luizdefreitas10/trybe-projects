ff811a883b94f033c2e10b35a3b8056804dfc615
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-07-11 18:40:27 -0300

    iniciando requisito 10

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Cart extends React.Component {
+  state = {
+    quantidade: [],
+  }
+
+  // counter= (event, product) => {
+  //   // const { cartList } = this.props;
+  //   const { value } = event.target;
+  //   // const { quantidade } = this.state;
+  //   if (value === '+') {
+  //     this.setState((prevState) => ({ quantidade: prevState.quantidade + 1 }));
+  //   } else {
+  //     this.setState((prevState) => ({ quantidade: prevState.quantidade - 1 }));
+  //   }
+  // }
+
+  counter = ({ target }) => {
+    if (target.value === '+') {
+      this.setState((get) => ({ quantidade: get.quantidade + 1 }));
+    } else {
+      this.setState((get) => ({ quantidade: get.quantidade - 1 }));
+    }
+  };
+
   render() {
     const { cartList } = this.props;
-    // console.log(cartList);
+    const { quantidade } = this.state;
     return (
       <div>
         { cartList.length === 0 ? (
@@ -14,6 +37,17 @@ class Cart extends React.Component {
             <div key={ index }>
               <h2 data-testid="shopping-cart-product-name">{ product.title }</h2>
               <h3>{product.price}</h3>
+              <input
+                type="button"
+                value="-"
+                onClick={ this.counter }
+              />
+              <p value={ quantidade }>{ quantidade }</p>
+              <input
+                type="button"
+                value="+"
+                onClick={ this.counter }
+              />
             </div>
           ))
         ) }
