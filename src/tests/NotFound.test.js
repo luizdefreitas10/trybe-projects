b1809c34ce0be6cd6559c22799e9f4d7a95afd2d
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-07-18 19:17:17 -0300

    concluindo requisito 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verificando o componente Not Found', () => {
+  it('se a página contém o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('se a imagem mostrada possui o src correto', () => {
+    renderWithRouter(<NotFound />);
+
+    const pikachuImg = screen.getByAltText(/pikachu crying/i);
+    expect(pikachuImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
