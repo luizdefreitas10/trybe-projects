f71c16a6c019b4d50b9a522da04b53a9d17fa394
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-07-18 18:12:54 -0300

    Concluindo requisito 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
-import { About } from '../pages';
+import { About, FavoritePokemons } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Verificando o componente About', () => {
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente Favorite Pokemons', () => {
+  it('se é exibido na tela o texto No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavText = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavText).toBeInTheDocument();
+  });
+});
