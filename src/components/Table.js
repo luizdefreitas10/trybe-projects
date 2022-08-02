0686174184ff5908203fabd890794d8d337172ce
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-08-01 22:42:45 -0300

    Concluindo requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { getFormState } = this.props;
     return (
-      <div>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        { getFormState.map((expense, index) => (
+          <tbody key={ index }>
+            <tr>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ Number(expense.value).toFixed(2) }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+              <td>
+                { (Number(expense.value) * (expense
+                  .exchangeRates[expense.currency].ask)).toFixed(2) }
+              </td>
+              <td>Real</td>
+
+            </tr>
+          </tbody>
+        )) }
+
+      </table>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  getFormState: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  getFormState: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Table);
